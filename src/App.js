df102f0a7ec42c5dc016de9ca410f69facd7270f
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-02-24 12:57:01 -0300

    Implementa validaçao do botao de salvar no formulario - Requisito 5

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import Card from './components/Card';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+      isSaveButtonDisabled: true,
+    };
 
     this.handleInput = this.handleInput.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -15,16 +25,31 @@ class App extends React.Component {
     const { name, type } = target;
     const value = type === 'checkbox' ? target.checked : target.value;
     this.setState({ [name]: value });
+    this.setState((prevState) => (type === 'number'
+      ? { isSaveButtonDisabled: !this.validAttrValues(prevState) }
+      : { isSaveButtonDisabled: !value.trim() }));
   }
 
   handleClick() {
     //
   }
 
+  validAttrValues({ cardAttr1, cardAttr2, cardAttr3 }) {
+    const maxSumValue = 210;
+    const maxAttrValue = 90;
+    const sum = parseInt(cardAttr1, 10) + parseInt(cardAttr2, 10)
+    + parseInt(cardAttr3, 10);
+
+    const validAttr = (Attr) => parseInt(Attr, 10) >= 0
+    && parseInt(Attr, 10) <= maxAttrValue;
+
+    return validAttr(cardAttr1) && validAttr(cardAttr2)
+    && validAttr(cardAttr3) && sum <= maxSumValue;
+  }
+
   render() {
-    const { cardName = '', cardDescription = '', cardAttr1 = '',
-      cardAttr2 = '', cardAttr3 = '', cardImage = '', cardRare = '',
-      cardTrunfo = false, isSaveButtonDisabled = false } = this.state;
+    const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
+      cardImage, cardRare, cardTrunfo, isSaveButtonDisabled } = this.state;
     return (
       <div>
         <Form
